5cd88b03773bfd39815efbcc7a18cc06f0d36025
IonelxSolow <ionelpotoroaca92@gmail.com>
2025-02-27 22:15:52 +0100

    add for loop to generate 10 random email

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,25 @@
-const emailListEl = document.getElementById('emailList');
-
-
 //event listener click del bottone
+document.getElementById('btnEmailGenerator').addEventListener('click', function() {   
+})
 
-
-// fetch
-fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+//ciclo for da eseguire 10 volte per creare le email
+for(let i = 0; i < 10; i++){
+    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
     .then(response => response.json())
     .then(data => {
-            //codice per far qualcosa con la risposta
-        console.log(data);
+        //create element li con le email
+        const li = document.createElement('li');
+        li.classList.add('list-group-item', 'list-group-item-dark')
+        li.textContent = data.response;
+        emailList.appendChild(li)
     })
     .catch(error => {
         // codice da eseguire in caso di errore
         console.error(error);
     });
-
-
-//ciclo for da eseguire 10 volte per creare le email
-
-for(let i = 0; i < 10; i++){
-    
 }
 
 
-//create element li con le email
+
 
 //stampare in pagina html la lista delle 10 email
